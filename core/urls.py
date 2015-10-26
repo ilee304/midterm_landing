9476226b87a0f637603c90f75accc1c8bc2d16d9
Isaiah <Isaiah.Lee@quinnipiac.edu>
2015-10-26 20:38:32 +0000

    Add success page

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from .views import *
 
 urlpatterns = patterns('',
-                      url(r'^$', Home.as_view(), name='home'),
-                      url(r'^question/create/$', QuestionCreateView.as_view(), name='question_create'),
-                       )
+     url(r'^$', Home.as_view(), name='home'),
+     url(r'^question/create/$', QuestionCreateView.as_view(), name='question_create'),
+     url(r'^success/$', Success.as_view(), name="success"),
+)
