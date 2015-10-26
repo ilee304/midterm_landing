45b052a47c1db6ea7a7cac9ab0d9bfd218346f82
Isaiah <Isaiah.Lee@quinnipiac.edu>
2015-10-26 17:36:23 +0000

    Fix in settings.py

--- midterm_landing/settings.py
+++ midterm_landing/settings.py
@@ -90,7 +90,7 @@ if ON_HEROKU == False:
 else:
   DATABASES = {}
   import dj_database_url
-  DATABASES['default'] = dj_database.url.config()
+  DATABASES['default'] = dj_database_url.config()
   SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
   ALLOWED_HOSTS - ['*']
 
